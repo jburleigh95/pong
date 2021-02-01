9ea561ffc77237435ee240b9ecd50642db139745
jburleigh95 <jburleigh77@gmail.com>
2021-01-31 22:53:43 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from turtle import Screen, Turtle
+from player import Player
+from ball import Ball
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.title('Pong')
+screen.tracer(0)
+
+r_paddle = Player((350, 0))
+l_paddle = Player((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.up, 'Up')
+screen.onkey(r_paddle.down, 'Down')
+screen.onkey(l_paddle.up, 'w')
+screen.onkey(l_paddle.down, 's')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    ball.move()
+
+    if ball.ycor() == 280 or ball.ycor() == -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+screen.exitonclick()
